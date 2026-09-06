919c16b08268e975e5d404006089b3b612a4703c
rodrigof981 <rodrigof981@gmail.com>
2026-09-06 18:51:38 -0300

    fix: Remover linha em branco desnecessária no PatientRelationshipController

--- backend/psicogest/psicogest/src/main/java/com/psicogest/psicogest/controller/PatientRelationshipController.java
+++ backend/psicogest/psicogest/src/main/java/com/psicogest/psicogest/controller/PatientRelationshipController.java
@@ -11,7 +11,6 @@ import java.util.List;
         "/patients/{patientId}/therapeutic-relationships"
 )
 public class PatientRelationshipController {
-
     private final TherapeuticRelationshipService
             relationshipService;
 
